7f408be01290b3ebe6a77a855c8d5cc4a3945557
Unknown <oblipn@gmail.com>
2017-05-22 22:23:37 +0200

    secure connexion incoming

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
+"use strict";
+
 /**
  * Load Conf file
  */
 const fs = require("fs");
 const path = require("path");
-var contents = fs.readFileSync(path.join(__dirname,"env.json"));
-var envSettings = JSON.parse(contents);
+const contents = fs.readFileSync(path.join(__dirname,"env.json"));
+const envSettings = JSON.parse(contents);
 console.log(envSettings.env+" env loaded");
 
 /**
- * Other Modules
+ * Server conf
  */
-if(envSettings.env == "prod" && typeof(envSettings.key) !== 'undefined' && typeof(envSettings.cert) !== 'undefined' ){ // Si on est en production on active https
+if(envSettings.env === "prod" && typeof(envSettings.key) !== 'undefined' && typeof(envSettings.cert) !== 'undefined' ){ // In production we activate SSL
     let options = {
         key: fs.readFileSync(path.join(__dirname,envSettings.key)),
         cert: fs.readFileSync(path.join(__dirname,envSettings.cert))
     };
-    var server = require('https').createServer(options, app);
-
-}else{ // Sinon on est en env de prod ou de test
-    var server = require('http').createServer(app);
+    var server = require('https').createServer(options);
+}else{ // Else int dev mode
+    var server = require('http').createServer();
 }
 
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
 /**
  * Start Socket.io
@@ -30,42 +31,95 @@ server.listen(envSettings.port);
 console.log("listenning on "+envSettings.port);
 
 /**
+ * Other
+ * SHA-512
+ * RSA-2048
+ */
+const sha512 = require('sha512');
+const nodeRSA = require('node-rsa');
+const keyRSA = new nodeRSA({b: 2048});
+
+/**
  * Chat
  */
 
 //Array with connected clients
-var clients = {};
+const clients = {};
 
-// add an item
-map[key1] = value1;
-// or remove it
-delete map[key1];
-// or determine whether a key exists
-key1 in map;
+// // add an item
+// map[key1] = value1;
+// // or remove it
+// delete map[key1];
+// // or determine whether a key exists
+// key1 in map;
+
+/**
+ * Data in sockets :
+ * socket.randToken : token
+ * socket.publicKey : RSA public Key under SHA-512
+ */
 
 io.on('connection', function(socket){
+
 
     /**
      * Connection
      */
-    socket.on('publickey', function(data){
-        console.log("someone connected");
-        clients[data.key] = socket;
-        socket.publicKey = data.key;
-        io.emit('alert', {
-            message : "Connected"
+    socket.on('connectStep1', function(){
+        console.log("someone is trying to connect");
+        // Sending client a random string (token)
+        socket.randToken = sha512(Date.now()+":"+Math.random()).toString('hex');
+        socket.emit('token', {
+            token : socket.randToken
         });
+    });
+    socket.on('connectStep2', function(data){
+
+        /**
+         * data :
+         * data.signedToken
+         * data.publicKey
+         */
+
+        if(typeof(data.signedToken) !== 'undefined' && typeof(data.publicKey) !== 'undefined'){
+            keyRSA.importKey(data.publicKey, "public");
+            if(keyRSA.verify(socket.randToken, data.signedToken)){
+
+                socket.publicKey = sha512(data.publicKey);
+                clients[socket.publicKey] = socket;
+
+                console.log("someone is connected");
+                io.emit('alert', {
+                    message : "Connected"
+                });
+            }else{
+                console.log("Token error at client connection");
+            }
+
+
+
+        }
     });
 
     /**
      * Message received
      */
     socket.on('message', function(data){
-        console.log("message sent");
-        io.emit('message', {
-            message : data.message,
-            pseudo : socket.name
-        });
+        // if(typeof(data.) !== 'undefined' && typeof(data.) !== 'undefined') {
+
+            console.log("message sent");
+
+            //Check the signed token
+
+            if (data.key in clients) {
+
+            }
+            io.emit('message', {
+                message: data.message,
+                pseudo: socket.name
+            });
+
+        // }
     });
 
     /**
